2eef45064207f5b15526974fee2f7bdc59dd818f
forloveone <1587007493@qq.com>
2019-08-29 16:50:12 +0800

    jmockit 单元测试框架,挺好用

---
+++ src/main/java/com/springboot/HelloJMockit.java
@@ -0,0 +1,36 @@
+package com.springboot;
+
+import lombok.Data;
+
+import java.util.Locale;
+@Data
+public class HelloJMockit {
+    private int i;
+
+    public static int j;
+
+    // 初始代码块
+    {
+        i = 1;
+    }
+
+    // 静态初始代码块
+    static {
+        j = 2;
+    }
+
+    public HelloJMockit(int i) {
+        this.i = i;
+    }
+
+    public String sayHello() {
+        Locale locale = Locale.getDefault();
+        if (locale.equals(Locale.CHINA)) {
+            // 在中国，就说中文
+            return "你好，JMockit!";
+        } else {
+            // 在其它国家，就说英文
+            return "Hello，JMockit!";
+        }
+    }
+}
